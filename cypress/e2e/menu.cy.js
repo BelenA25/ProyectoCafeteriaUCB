bcab344c7b29ea5cfac7973fe924abc38c4a4066
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-15 19:57:26 -0400

    primera prueba de interfaz de usuario (sopa de mani)

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -1,5 +1,5 @@
 describe("menu", () => {
-    it("deberia mostrar todo el menu disponible", () => {
+    it("deberia mostrar todo el menu disponible ", () => {
       cy.visit("/");
     //   cy.get("#primer-numero").type(4);
     //   cy.get("#segundo-numero").type(5);
@@ -10,3 +10,14 @@ describe("menu", () => {
     });
   });
   
+  describe("menu", () => {
+    it("deberia mostrar todo el menu disponible (sopa de mani)", () => {
+      cy.visit("/");
+    //   cy.get("#primer-numero").type(4);
+    //   cy.get("#segundo-numero").type(5);
+    //   cy.get("#sumar-button").click();
+    //   cy.get("#resultado-div").should("contain", "9");
+         cy.get("div").should("contain", "Sopa de mani");
+        
+    });
+  });
