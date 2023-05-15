0e8da2c81693a4221146079c09adf3bad2bd766b
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-15 19:40:58 -0400

    primera prueba de interfaz de usuario (menu cafeteria)

---
+++ cypress/e2e/menu.cy.js
@@ -0,0 +1,12 @@
+describe("menu", () => {
+    it("deberia mostrar todo el menu disponible", () => {
+      cy.visit("/");
+    //   cy.get("#primer-numero").type(4);
+    //   cy.get("#segundo-numero").type(5);
+    //   cy.get("#sumar-button").click();
+    //   cy.get("#resultado-div").should("contain", "9");
+         cy.get("div").should("contain", "Silpancho");
+        
+    });
+  });
+  
