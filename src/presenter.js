faae74831cee7a6e791cc70ffd34594e3a2e36b1
“Belen <beckiar23@gmail.com>
2023-05-16 14:33:26 -0400

    Refactorign: Se agrego el uso de la clase menu para obtener el stock en la interfaz de usuario

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,19 @@
 import Menu from "./menu.js";
 
 const form = document.querySelector("#menu-form");
-const div = document.querySelector("#sopa");
-const div2 = document.querySelector("#segundo");
+const divSopa = document.querySelector("#sopa");
+const divSegundo = document.querySelector("#segundo");
+const divStockSopa = document.querySelector("#stock-sopa");
+const divStockSegundo = document.querySelector("#stock-segundo");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const menu = new Menu("Sopa de mani", "Silpancho");
+  menu.agregarStockSegundo(20);
+  menu.agregarStockSopa(15);
   console.log(menu.sopa)
-  div.innerHTML = menu.sopa;
-  div2.innerHTML = menu.segundo;
+  divSopa.innerHTML = menu.sopa;
+  divSegundo.innerHTML = menu.segundo;
+  divStockSopa.innerHTML = "(Stock = "+menu.stockSopa+")";
+  divStockSegundo.innerHTML = "(Stock = "+menu.stockSegundo+")";
 });
