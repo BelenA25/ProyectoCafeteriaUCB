29de2d43602713e5265fc3478ecadd62ac3e06cf
Belen <beckiar23@gmail.com>
2023-05-16 10:00:03 -0400

    Se agregaron mas pruebas para comprobar el menu del dia

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -1,23 +1,25 @@
-describe("menu", () => {
-    it("deberia mostrar todo el menu disponible ", () => {
-      cy.visit("/");
-    //   cy.get("#primer-numero").type(4);
-    //   cy.get("#segundo-numero").type(5);
-    //   cy.get("#sumar-button").click();
-    //   cy.get("#resultado-div").should("contain", "9");
-         cy.get("div").should("contain", "Silpancho");
-        
-    });
+describe("Menu", () => {
+  it("deberia mostrar todo el menu disponible ", () => {
+    cy.visit("/");
+    cy.get("div").should("contain", "Silpancho");
+
   });
-  
-  describe("menu", () => {
-    it("deberia mostrar todo el menu disponible (sopa de mani)", () => {
-      cy.visit("/");
-    //   cy.get("#primer-numero").type(4);
-    //   cy.get("#segundo-numero").type(5);
-    //   cy.get("#sumar-button").click();
-    //   cy.get("#resultado-div").should("contain", "9");
-         cy.get("div").should("contain", "Sopa de mani");
-        
-    });
+  it("deberia mostrar todo el menu disponible (sopa de mani)", () => {
+    cy.visit("/");
+    cy.get("div").should("contain", "Sopa de mani");
+
   });
+});
+
+describe("Menu y precio", () => {
+  it("deberia mostrar el precio de cada elemento del menu", () => {
+    cy.visit("/");
+    cy.get("div").should("contain", "Solo Sopa = 5 Bs.");
+
+  });
+  it("deberia mostrar el precio de cada elemento del menu", () => {
+    cy.visit("/");
+    cy.get("div").should("contain", "Solo Segundo = 13 Bs.");
+
+  });
+});
