0edde0a4a88bdc39f807896fdb4b377ba281ffee
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-17 15:58:24 -0400

    prueba de interfaz de usuario deberia permitir editar los precios del menu

--- cypress/e2e/editarMenu.cy.js
+++ cypress/e2e/editarMenu.cy.js
@@ -5,5 +5,18 @@ describe("Editar Menu", () => {
         cy.get("#stock-segundo").should("have.attr", "data-stock", "0");
         cy.get("#reservas-segundo").should("contain", "Ya no se permiten más reservas");
       });
-
+    it("debería permitir editar los precios del menú", () => {
+        cy.visit("/");
+        const nuevoPrecioSopa = "8 Bs.";
+        const nuevoPrecioSegundo = "15 Bs.";
+        const nuevoPrecioAlmuerzoCompleto = "18 Bs.";
+        cy.get("div:contains('Solo Sopa')").invoke("text", `Solo Sopa = ${nuevoPrecioSopa}`);
+        cy.get("div:contains('Solo Segundo')").invoke("text", `Solo Segundo = ${nuevoPrecioSegundo}`);
+        cy.get("div:contains('Almuerzo completo')").invoke("text", `Almuerzo completo = ${nuevoPrecioAlmuerzoCompleto}`);
+        cy.get("div:contains('Solo Sopa')").should("contain", `Solo Sopa = ${nuevoPrecioSopa}`);
+        cy.get("div:contains('Solo Segundo')").should("contain", `Solo Segundo = ${nuevoPrecioSegundo}`);
+        cy.get("div:contains('Almuerzo completo')").should("contain", `Almuerzo completo = ${nuevoPrecioAlmuerzoCompleto}`);
+    });
+      
+      
 });
