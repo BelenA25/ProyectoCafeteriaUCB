ad10e4ccba150476467ef6b6e7feb792d364c5a2
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-17 16:54:21 -0400

    prueba de interfaz de usuario deberia modificar el stock del menu

--- cypress/e2e/editarMenu.cy.js
+++ cypress/e2e/editarMenu.cy.js
@@ -27,6 +27,21 @@ describe("Editar Menu", () => {
         cy.get("div:contains('Solo Segundo')").invoke("text", `Solo ${nuevoNombreSegundo} = 13 Bs.`).should("contain", `Solo ${nuevoNombreSegundo} = 13 Bs.`);
         cy.get("div:contains('Almuerzo completo')").invoke("text", `${nuevoNombreAlmuerzoCompleto} = 16 Bs.`).should("contain", `${nuevoNombreAlmuerzoCompleto} = 16 Bs.`);
       });
+      it("debería modificar el stock del menú", () => {
+        cy.visit("/");
+        const nuevoStockSopa = 10;
+        const nuevoStockSegundo = 8;
+        cy.get("#stock-sopa").then(($stockSopa) => {
+          $stockSopa[0].innerHTML = `Stock: ${nuevoStockSopa} <span id="stock-value-sopa"></span>`;
+        });
+        cy.get("#stock-segundo").then(($stockSegundo) => {
+          $stockSegundo[0].innerHTML = `Stock: ${nuevoStockSegundo} <span id="stock-value-segundo"></span>`;
+        });
+        cy.get("#stock-sopa").should("contain", `Stock: ${nuevoStockSopa}`);
+        cy.get("#stock-segundo").should("contain", `Stock: ${nuevoStockSegundo}`);
+      });
+      
+      
       
       
       
