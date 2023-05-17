f425a4473993f5d808e833c78330fc69fa0a4366
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-17 00:08:32 -0400

    prueba interfaz de usuario debería verificar el stock de un plato específico después de agregar reservas

--- cypress/e2e/crearMenu.cy.js
+++ cypress/e2e/crearMenu.cy.js
@@ -1,8 +1,21 @@
-it("debería agregar una reserva y verificar el stock actualizado", () => {
-    cy.visit("/");
-    cy.get("#mostrar-button").click();
-    cy.get("div").should("contain", "Silpancho");
-    cy.get("#reservar-segundo-button").click(); 
-    cy.get("#stock-segundo").should("have.attr", "data-stock", "4");
+describe("Crear Menu", () => {
+    it("debería agregar una reserva y verificar el stock actualizado", () => {
+        cy.visit("/");
+        cy.get("#mostrar-button").click();
+        cy.get("div").should("contain", "Silpancho");
+        cy.get("#reservar-segundo-button").click(); 
+        cy.get("#stock-segundo").should("have.attr", "data-stock", "4");
+      });
+      it("debería verificar el stock de un plato específico después de agregar reservas", () => {
+        cy.visit("/");
+        cy.get("#mostrar-button").click();
+        cy.get("div").should("contain", "Silpancho");
+        cy.get("#reservar-segundo-button").click();
+        cy.get("#stock-segundo").should("have.attr", "data-stock").then((stock) => {
+          const stockValue = parseInt(stock);
+          expect(stockValue).to.be.greaterThan(0);
+        });
+      });
+      
   });
-  
+
