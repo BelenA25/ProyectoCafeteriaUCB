5d80baca1042077a7c9d78371215d278ec2a55b6
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-17 21:08:27 -0400

    prueba de interfaz de usuario deberia mostrar el formulario de crear menu

--- cypress/e2e/crearMenu.cy.js
+++ cypress/e2e/crearMenu.cy.js
@@ -1,29 +1,27 @@
 describe("Crear Menu", () => {
-    it("debería agregar una reserva y verificar el stock actualizado", () => {
-        cy.visit("/");
-        cy.get("#mostrar-button").click();
-        cy.get("div").should("contain", "Silpancho");
-        cy.get("#reservar-segundo-button").click(); 
-        cy.get("#stock-segundo").should("have.attr", "data-stock", "4");
-      });
-      it("debería verificar el stock de un plato específico después de agregar reservas", () => {
-        cy.visit("/");
-        cy.get("#mostrar-button").click();
-        cy.get("div").should("contain", "Silpancho");
-        cy.get("#reservar-segundo-button").click();
-        cy.get("#stock-segundo").should("have.attr", "data-stock").then((stock) => {
-          const stockValue = parseInt(stock);
-          expect(stockValue).to.be.greaterThan(0);
-        });
-      });
-    //   it("debería mostrar un mensaje de reserva agotada después de alcanzar el límite de stock", () => {
+    // it("debería agregar una reserva y verificar el stock actualizado", () => {
     //     cy.visit("/");
     //     cy.get("#mostrar-button").click();
-    //     cy.get("div").should("contain", "Sopa de mani");
-    //     cy.get("#reservar-sopa-button").click({ multiple: true, force: true });
-    //     cy.get("#reservas-sopa").should("contain", "Ya no se permiten más reservas");
+    //     cy.get("div").should("contain", "Silpancho");
+    //     cy.get("#reservar-segundo-button").click(); 
+    //     cy.get("#stock-segundo").should("have.attr", "data-stock", "4");
     //   });
-      
-      
+    //   it("debería verificar el stock de un plato específico después de agregar reservas", () => {
+    //     cy.visit("/");
+    //     cy.get("#mostrar-button").click();
+    //     cy.get("div").should("contain", "Silpancho");
+    //     cy.get("#reservar-segundo-button").click();
+    //     cy.get("#stock-segundo").should("have.attr", "data-stock").then((stock) => {
+    //       const stockValue = parseInt(stock);
+    //       expect(stockValue).to.be.greaterThan(0);
+    //     });
+    //   });
+
+        it("debería mostrar el formulario de creacion de menu", () => {
+        cy.visit("/");
+        cy.get("#crear-sopa").should("contain","Crear sopa:");
+           
+      });
+
   });
 
