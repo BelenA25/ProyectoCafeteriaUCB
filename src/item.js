a7845e6b51609ab49736a43bf07017d7032caed5
libia <libia.roman@ucb.edu.bo>
2023-05-25 16:23:09 +0200

    Se anadio la clase Item que incluye cada elemento de la cafeteria

---
+++ src/item.js
@@ -0,0 +1,34 @@
+class Item {
+    constructor(nombre, descripcion, precio, categoria, stock) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = stock;
+        this.reservas = 0;
+    }
+    agregarReserva(reserva) {
+        if (this.reservas < this.stock) {
+            this.reservas = this.reservas + reserva;
+        }
+    }
+    decrementarReservas(reserva) {
+        this.reservas = this.reservas - reserva;
+    }
+    actualizarNombre(nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+    actualizarDescripcion(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+    actualizarPrecio(nuevoPrecio) {
+        this.precio = nuevoPrecio;
+    }
+    actualizarCategoria(nuevaCategoria) {
+        this.categoria = nuevaCategoria;
+    }
+    actualizarStock(nuevoStock) {
+        this.stock = nuevoStock;
+    }
+}
+export default Item;
---
+++ src/items.test.js
@@ -0,0 +1,12 @@
+import Item from "./item.js";
+
+describe("Crear item", () => {
+    it("debería crear un item de la cafeteria", () => {
+        const item = new Item("Sopa", "Sopa de verduras", 5, "Almuerzo cotidiano", 25);
+        expect(item.nombre).toBe("Sopa");
+        expect(item.descripcion).toBe("Sopa de verduras");
+        expect(item.precio).toBe(5);
+        expect(item.categoria).toBe("Almuerzo cotidiano");
+        expect(item.stock).toBe(25);
+    });
+});
