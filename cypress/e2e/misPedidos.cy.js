5cda5697f90c52ecd1f474beae5833645ff64bb3
“Belen <beckiar23@gmail.com>
2023-05-24 20:34:10 -0400

    Refactoring: prueba de interfaz de usuario corregida

--- cypress/e2e/misPedidos.cy.js
+++ cypress/e2e/misPedidos.cy.js
@@ -1,4 +1,5 @@
 describe("Mis pedidos", () => {
+
   it("deberia mostrar el apartado de mis pedidos", () => {
     cy.visit("/");
     cy.get("#tittle-mis-pedidos").should("contain", "Mis pedidos");
@@ -10,9 +11,8 @@ describe("Mis pedidos", () => {
     cy.get("#input-sopa-stock-creada").type(200);
     cy.get("#input-segundo-stock-creada").type(30);
     cy.get("#crear-menu-button").click();
-    cy.get("#input-reserva-sopa").type(5);
-    cy.get("#reserva-hecha-button").click();
-    cy.get("#mi-pedido-sopa").should("contain", "Sopa: 5");
+    cy.get("#boton-reserva-sopa").click(1);
+    cy.get("#mi-pedido-sopa").should("contain", "Sopa: 1");
   });
   it("deberia eliminar mi pedido", () => {
     cy.visit("/");
@@ -21,8 +21,7 @@ describe("Mis pedidos", () => {
     cy.get("#input-sopa-stock-creada").type(200);
     cy.get("#input-segundo-stock-creada").type(30);
     cy.get("#crear-menu-button").click();
-    cy.get("#input-reserva-sopa").type(5);
-    cy.get("#reserva-hecha-button").click();
+    cy.get("#boton-reserva-sopa").click();
     cy.get("#eliminar-button").click();
     cy.get("#mi-pedido-sopa").should("contain", "Sopa: 0");
   });
