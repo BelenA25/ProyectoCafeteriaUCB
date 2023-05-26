2abf3b0fa1a588bcf1127d80fd0bf8064f48a4a9
Grover <grover.gonzalez@ucb.edu.bo>
2023-05-25 23:08:32 -0400

    Haciendo refactoring en el editarMenu

--- cypress/e2e/editarMenu.cy.js
+++ cypress/e2e/editarMenu.cy.js
@@ -1,22 +1,12 @@
 describe("Editar Menu", () => {
-    // it("debería mostrar un mensaje cuando se agota el stock del segundo", () => {
-    //     cy.visit("/");
-
-    //     cy.get("#stock-segundo").invoke("attr", "data-stock", "10");
-    //     cy.get("#stock-segundo").should("have.attr", "data-stock", "0");
-    //     cy.get("#reservas-segundo").should("contain", "Ya no se permiten más reservas");
-    //   });
     it("debería permitir editar los precios del menú", () => {
         cy.visit("/");
         const nuevoPrecioSopa = "8 Bs.";
         const nuevoPrecioSegundo = "15 Bs.";
-        const nuevoPrecioAlmuerzoCompleto = "18 Bs.";
         cy.get("div:contains('Solo Sopa')").invoke("text", `Solo Sopa = ${nuevoPrecioSopa}`);
         cy.get("div:contains('Solo Segundo')").invoke("text", `Solo Segundo = ${nuevoPrecioSegundo}`);
-        cy.get("div:contains('Almuerzo completo')").invoke("text", `Almuerzo completo = ${nuevoPrecioAlmuerzoCompleto}`);
         cy.get("div:contains('Solo Sopa')").should("contain", `Solo Sopa = ${nuevoPrecioSopa}`);
         cy.get("div:contains('Solo Segundo')").should("contain", `Solo Segundo = ${nuevoPrecioSegundo}`);
-        cy.get("div:contains('Almuerzo completo')").should("contain", `Almuerzo completo = ${nuevoPrecioAlmuerzoCompleto}`);
     });
     it("debería permitir editar el nombre de los platos del menú", () => {
         cy.visit("/");
@@ -26,7 +16,6 @@ describe("Editar Menu", () => {
       
         cy.get("div:contains('Solo Sopa')").invoke("text", `Solo ${nuevoNombreSopa} = 5 Bs.`).should("contain", `Solo ${nuevoNombreSopa} = 5 Bs.`);
         cy.get("div:contains('Solo Segundo')").invoke("text", `Solo ${nuevoNombreSegundo} = 13 Bs.`).should("contain", `Solo ${nuevoNombreSegundo} = 13 Bs.`);
-        cy.get("div:contains('Almuerzo completo')").invoke("text", `${nuevoNombreAlmuerzoCompleto} = 16 Bs.`).should("contain", `${nuevoNombreAlmuerzoCompleto} = 16 Bs.`);
       });
       it("debería modificar el stock del menú", () => {
         cy.visit("/");
