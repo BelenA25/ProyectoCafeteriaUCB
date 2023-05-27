b091573c3e9853882128f290309324cf1b401ebf
Grover <grover.gonzalez@ucb.edu.bo>
2023-05-27 16:24:23 -0400

    Creando pruebas de crypress y modificando el package

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,15 @@
+describe('Inicio de Sesión', () => {
+    it('Iniciar sesión exitosamente', () => {
+        cy.visit('login.html')
+        // Ingresa el nombre de usuario
+        cy.get('#inputUsername').type('miusuario')
+        // Ingresa la contraseña
+        cy.get('#inputpassword').type('micontraseña')    
+    });
 
-  
+    it('Mostrar mensaje de error en caso de inicio de sesión incorrecto', () => {
+        cy.visit('login.html')
+        cy.get('#inputUsername').type('nombre_de_usuario_incorrecto')
+        cy.get('#inputpassword').type('contraseña_incorrecta')
+    });
+});
