84a4e0ec4c5a39a4a0733aed68ea470e8a1ef711
Grover <grover.gonzalez@ucb.edu.bo>
2023-05-27 16:36:26 -0400

    Prueba iniciando sesion con la funcion loginUser

--- src/User.js
+++ src/User.js
@@ -3,6 +3,10 @@ class User{
         this.username = username;
         this.password = password;
     }
+
+    loginUser() {
+        return {"username":this.username,"password":this.password};
+    }
 }
 
 export default User;
