ed4d68f150ca50a2d1912d1431250e14511d4b05
Grover <grover.gonzalez@ucb.edu.bo>
2023-05-29 16:07:39 -0400

    Creando los archivos de login necesarios y modificando un poco de codigo

--- src/Admin.js
+++ src/Admin.js
@@ -1,7 +1,7 @@
 class Admin{
     constructor() {
-        this.username = "admin"
-        this.password = "password"
+        this.username = "admin";
+        this.password = "password";
     }
     loginAdmin() {
         return {"username":this.username,"password":this.password};
