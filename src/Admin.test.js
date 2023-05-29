e786ea51a3271e7f9450769ce32cc0b4fc58ff0c
Grover <grover.gonzalez@ucb.edu.bo>
2023-05-29 15:57:45 -0400

    Creando el primer test y archivos necesarios para login como admin

---
+++ src/Admin.test.js
@@ -0,0 +1,6 @@
+describe("Iniciar Sesion como Admin", () => {
+    var admin = new Admin();
+    it("Deberia iniciar sesion con el nombre de usuario", () => {
+        expect(admin.username).toEqual("admin");
+    });
+});
