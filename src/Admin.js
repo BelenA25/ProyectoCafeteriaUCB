b905f93a6bf1dccc04ab044d931354b3aa76fd74
Grover <grover.gonzalez@ucb.edu.bo>
2023-05-29 16:04:13 -0400

    Pasando el cuarto test utilizando la funcion loginAdmin

--- src/Admin.js
+++ src/Admin.js
@@ -2,7 +2,10 @@ class Admin{
     constructor() {
         this.username = "admin"
         this.password = "password"
-      }
+    }
+    loginAdmin() {
+        return {"username":this.username,"password":this.password};
+    }
 }
 
 export default Admin;
