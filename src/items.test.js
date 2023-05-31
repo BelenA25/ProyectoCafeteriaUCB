a20bd156e1fc14e7f55b5fcb1683a09b4d21ccf1
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-30 22:41:23 -0400

    Prueba deberia eliminar un producto correctamente cuando hay varios en la lista

--- src/items.test.js
+++ src/items.test.js
@@ -12,6 +12,9 @@ describe("Crear item", () => {
 });
 
 describe("Eliminar Item", () => {
+    beforeEach(() => {
+        Item.items = []; 
+      });
     it("debería eliminar un producto creado", () => {
       const item = new Item(1, "Papitas", "Picantes", 10, "Frituras", 5);
       Item.items.push(item);
@@ -26,5 +29,15 @@ describe("Eliminar Item", () => {
         const itemNoExistente = new Item(2, "Galletas", "Vainilla", 8, "Dulces", 3);
         itemNoExistente.eliminar();
         expect(Item.items.length).toBe(1);
+      });
+      it("debería eliminar el producto correcto cuando hay varios en la lista", () => {
+        const item1 = new Item(1, "Papitas", "Picantes", 10, "Frituras", 5);
+        const item2 = new Item(2, "Galletas", "Vainilla", 8, "Dulces", 3);
+        const item3 = new Item(3, "Refresco", "Cola", 15, "Bebidas", 7);
+        Item.items.push(item1, item2, item3);
+        expect(Item.items.length).toBe(3);
+        item2.eliminar();
+        expect(Item.items.length).toBe(2);
+        expect(Item.items).not.toContain(item2);
       });
   });
