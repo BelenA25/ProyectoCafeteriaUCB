2b9b395cf009f3eccf9d27cfa06eab6a220f7742
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-05-31 00:22:09 -0400

    pruebas de interfaz de usuario: deberia mostrarse aun el item si cancele la eliminacion del mismo

--- cypress/e2e/eliminarItem.cy.js
+++ cypress/e2e/eliminarItem.cy.js
@@ -61,4 +61,27 @@ describe("Eliminar Item", () => {
           cy.get("@alertStub").should("be.calledWith", "La eliminación del item ha sido cancelada");
         });
       });
+      it("debería mostrarse aun el item al cancelar la eliminacion de la misma", () => {
+        cy.visit("/");
+        cy.get("#inputUsername").type("admin");
+        cy.get("#inputpassword").type("password");
+        cy.get("#login-button").click();
+      
+        cy.get("#input-nombre-creada").type("Sopa");
+        cy.get("#input-descripcion-creada").type("Sopa de fideo");
+        cy.get("#input-precio-creada").type(5);
+        cy.get("#input-categoria-creada").type("Almuerzo cotidiano");
+        cy.get("#input-stock-creada").type(20);
+        cy.get("#crear-menu-button").click();
+      
+        cy.window().then((win) => {
+          cy.stub(win, "confirm").returns(false); 
+          cy.stub(win, "alert").as("alertStub"); 
+      
+          cy.get(".eliminar-item").click(); 
+          cy.get("@alertStub").should("be.calledWith", "La eliminación del item ha sido cancelada");
+          cy.get("#mostrar-items").should("contain", "Sopa");
+
+        });
+      });
   });
